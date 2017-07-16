dbc61784288bc7e8ca914502f011b892f711cd96
Shahzaib Ather <shahzaibather7@gmail.com>
2017-07-16 13:36:57 -0600

    Add files via upload

---
+++ 2nd Attempt Assignment 4/Header.h
@@ -0,0 +1,17 @@
+//
+//  Header.h
+//  2nd Attempt Assignment 4
+//
+//  Created by Mohammad Shahzaib Ather on 2017-07-16.
+//  Copyright © 2017 Mohammad Shahzaib Ather. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+@interface SampleClass : NSObject ;
+
+-(void) largeNumMethod : (NSArray *) arr ;
+@end
+
+#endif /* Header_h */
--- 2nd Attempt Assignment 4/main.m
+++ 2nd Attempt Assignment 4/main.m
@@ -1,17 +1,35 @@
 //
 //  main.m
-//  2nd Attempt Assignment 4
+//  Assignment 4
 //
 //  Created by Mohammad Shahzaib Ather on 2017-07-13.
 //  Copyright © 2017 Mohammad Shahzaib Ather. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import "Header.h"
+
+
+@implementation SampleClass
+
+-(void) largeNumMethod : (NSArray *) arr ;
+{
+    NSNumber *largeNum = [arr valueForKeyPath:@"@max.intValue"];
+    NSLog(@"The largest number in the array is: %@" , largeNum);
+}
+@end
+
+    
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+      
+        NSArray *array = @[@44, @5, @6];
+        SampleClass *sampleclass = [[SampleClass alloc] init];
+        [sampleclass largeNumMethod:array];
+        
+        
     }
     return 0;
 }
+
